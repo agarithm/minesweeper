9838bbb0732e3700f417f8d432fb38fe4b41b5cc
Mike Agar <github@jmagar.com>
2012-12-29 14:55:26 -0500

    baby steps

---
+++ ie_warn.php
@@ -0,0 +1,7 @@
+<h1>Browser Not Supported</h1>
+<p>
+The browser you are using is not supported by this application.  Please consider upgrading to a newer, more capable, browser.  Here are two equally good choices:
+<ul>
+<li>Google Chrome: <a href=http://chrome.google.com>DOWNLOAD</a></li>
+<li>Firefox: <a href=http://download.cnet.com/mozilla-firefox/>DOWNLOAD</a></li>
+</ul>
---
+++ minesweeper.php
@@ -0,0 +1,28 @@
+<div id="block">
+    <h1>JavaScript Minesweeper</h1>
+    <div id="mines_block">
+		<div id="mines_controls">
+		</div>
+	    <div id="mines_board">    
+	    </div>
+	</div>
+</div>
+<div id="block">
+    <h1>The JavaScript Minesweeper Story</h1>
+    <h2>Goal</h2>
+    <h2>Resources</h2>
+        <ul>
+            <li><a href="http://www.chezpoor.com/minesweeper/minesweeper.html" target="new" >A near perfect replication of the MS version of the game.</a></li> 
+            <li><a href="minesweeper/HTML5_Canvas_Cheat_Sheet.png" target="new">Canvas Cheat Sheet</a></li>
+        </ul>
+    <h2>Ideas:</h2>
+        <ul>
+        <li>Use canvas and ssvg like graphics to make the images. No PNG or GIf or JPG anywhere</li>
+        <li>Make it touch friendly: No right click for flags.  Solve this UI problem...</li>
+        </ul>
+     <h2>Log:</h2>
+        <h3>2012</h3>
+        <ul>
+        <li>Dec 29: Create a GIT repo for the project code</li>
+        </ul>
+</div>
